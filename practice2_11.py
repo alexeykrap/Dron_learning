8b33e2e1f9bcbd6bfd511f48137fb1da7040ea0a
alexeykrap <49550836+alexeykrap@users.noreply.github.com>
2024-07-26 15:35:04 +0300

    паттерны для работы с БД №1

--- practice2_11.py
+++ practice2_11.py
@@ -11,7 +11,8 @@ class DBFactory(ABC):
 
 class SQLiteDBFactory(DBFactory):
     def connect(self):
-        return sqlite3.connect(':memory:')  # ':memory:' означает, что база данных создаётся в оперативной памяти
+        return sqlite3.connect(':memory:')
+        # ':memory:' вместо пути к файлу означает, что база данных создаётся в оперативной памяти
 
 
 # pattern Builder
@@ -26,8 +27,7 @@ class QueryBuilder:
             'insert_into': None,
             'values': None,
         }
-
-        self._prams = []
+        self._params = []
 
     def select(self, table, columns='*'):
         self._query['select'] = f'SELECT {columns} '
@@ -35,21 +35,22 @@ class QueryBuilder:
         return self
 
         # tbl_drone -> id, model, manufacturer
-        # SELECT * FROM tbl_drone
-        # SELECT manufacturer FROM tbl_drone
+        # SELECT * FROM tbl_drones
+        # SELECT manufacturer FROM tbl_drones
 
-    def where(self, condition, parametrs=None):
+    def where(self, condition, parameters=None):
         self._query['where'] = f'WHERE {condition} '
-        if parametrs:
-            self._prams.extend(parametrs)
+
+        if parameters:
+            self._params.extend(parameters)
         return self
 
     def order_by(self, order):
         self._query['order_by'] = f'ORDER BY {order} '
         return self
 
-    def add_params(self, *parametrs):
-        self._prams.extend(parametrs)
+    def add_params(self, *parameters):
+        self._params.extend(parameters)
         return self
 
     def insert_into(self, table, columns):
@@ -67,7 +68,7 @@ class QueryBuilder:
         # execute("INSERT INTO tbl_drones (models, manufacturer) VALUES (?,?)", ("model x", "FlyCorp"))
 
     def values(self, *values):
-        self._prams.extend(values)
+        self._params.extend(values)
         return self
 
     def get_query(self):
@@ -83,7 +84,23 @@ class QueryBuilder:
         return query
 
     def get_params(self):
-        return self._prams
+        return self._params
+
+
+class Drone:
+    def __init__(self, model, manufacturer, year):
+        self._model = model
+        self._manufacturer = manufacturer
+        self._year = year
+
+    def get_model(self):
+        return self._model
+
+    def get_manufacturer(self):
+        return self._manufacturer
+
+    def get_year(self):
+        return self._year
 
 
 if __name__ == "__main__":
@@ -100,29 +117,45 @@ if __name__ == "__main__":
         )
     """)
 
-    drone = {
-        "model": "model x",
-        "manufacturer": "SkyCorp",
-        "year": "2024",
-    }  # в качестве домашнего задания сделать таблицу в базе данных
+    # drone = {
+    #     "model": "model x",
+    #     "manufacturer": "SkyCorp",
+    #     "year": "2024",
+    # }  # в качестве домашнего задания сделать таблицу в базе данных
+
+    my_drone = Drone('Model X', 'FlyCorp', '2024')
+    my_drone2 = Drone("Model Y", "NewCorp", "2025")
 
     query_builder = QueryBuilder()
     insert_into = query_builder.insert_into("tbl_drones", ['model', 'manufacturer', 'year'])\
-        .values(drone['model'], drone['manufacturer'], drone['year']).get_query()
+        .values(my_drone.get_model(), my_drone.get_manufacturer(), my_drone.get_year()).get_query()
     print(insert_into)
+
+    query_builder2 = QueryBuilder()
+    insert_into2 = query_builder2.insert_into("tbl_drones", ["model", "manufacturer", "year"])\
+        .values(my_drone2.get_model(), my_drone2.get_manufacturer(), my_drone2.get_year()).get_query()
+    print(insert_into2)
 
     params = query_builder.get_params()
     cursor.execute(insert_into, params)
     connection.commit()
 
+    # Вывод сформированного запроса
+    params2 = query_builder2.get_params()  # Получение параметров для запроса WHERE
+    cursor.execute(insert_into2, params2)  # Выполнение запроса с параметрами
+    connection.commit()
+
+    # Создание SELECT-запроса с использованием нового экземпляра QueryBuilder
     select_query = QueryBuilder()
     select_query = select_query.select('tbl_drones').get_query()
     cursor.execute(select_query)
     results = cursor.fetchall()
+
+    # Вывод всех записей из таблицы
     for row in results:
         print(row)
 
-    connection.close()
+    connection.close()  # Закрытие подключения к базе данных
 
 
 
